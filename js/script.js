59b747628b398d761f813d87db10b40c1f1b0d2e
Ivan Klimukhin <ivankmn@yandex.ru>
2019-12-06 22:58:09 +0300

    добавил адаптив

--- js/script.js
+++ js/script.js
@@ -10,4 +10,26 @@ $(document).ready(function () {
     $('.nav-overlay').removeClass('nav-overlay--open');
     $('body').removeClass('modal-open');
   });
+
+  $('.seeyourself').on('click', function() {
+    $(this).toggleClass('seeyourself--opened');
+    $('.add-text').slideToggle();
+  });
+
+  $('.accordion__title').on('click', function(evt) {
+    evt.preventDefault();
+    if ($(this).hasClass('accordion__title--active')) {
+      $(this).removeClass('accordion__title--active');
+      $(this).siblings('.accordion__content').slideUp(200);
+      $('.accordion__btn').removeClass('accordion__btn--minus').addClass('accordion__btn--plus');
+    } else {
+      $('.accordion__btn').removeClass('accordion__btn--minus').addClass('accordion__btn--plus');
+      $(this).find('.accordion__btn').removeClass('accordion__btn--plus').addClass('accordion__btn--minus');
+      $('.accordion__title').removeClass('accordion__title--active');
+      $(this).addClass('accordion__title--active');
+      $('.accordion__content').slideUp(200);
+      $(this).siblings('.accordion__content').slideDown(200);
+    }
+  });
+  $('.accordion .accordion__item').first().children('.accordion__title').trigger('click');
 });
